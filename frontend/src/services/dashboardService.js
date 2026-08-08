0b418a8727f3d23ce639a7033621b1da39336be5
Malay Barochiya <malaybarochiya@gmail.com>
2026-08-08 14:51:18 +0530

    Responsive layout update

--- frontend/src/services/dashboardService.js
+++ frontend/src/services/dashboardService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   import.meta.env.VITE_API_URL ||
-  "http://localhost:5000";
+  "https://om-tiffin-management-system.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,

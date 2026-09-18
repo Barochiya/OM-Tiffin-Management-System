cb42c5a56f801ebcf3018be7d6470b2dc2917f82
Malay Barochiya <malaybarochiya@gmail.com>
2026-09-18 20:19:42 +0530

    add customer protected route

---
+++ frontend/src/components/CustomerProtectedRoute.jsx
@@ -0,0 +1,9 @@
+﻿import { Navigate, Outlet } from "react-router-dom";
+const CustomerProtectedRoute = () => {
+  const token = sessionStorage.getItem("customerToken");
+  if (!token) {
+    return <Navigate to="/customer-login" replace />;
+  }
+  return <Outlet />;
+};
+export default CustomerProtectedRoute;
